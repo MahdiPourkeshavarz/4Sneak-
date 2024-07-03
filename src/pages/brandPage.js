264149c9dc4d09ccb863b165e37eeff7a46dcb8c
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-07-03 22:29:19 +0330

    add : brand page ui and logic for fetching data

--- src/pages/brandPage.js
+++ src/pages/brandPage.js
@@ -1,6 +1,45 @@
+import { MOST_URL, NIKE_PRO, PRODUCT_URL } from "../services/links";
+import { getProducts } from "./homePage";
 
 const container = document.getElementById('app');
 
+let brand = "Adidas";
+
+let URL = "";
+
+export function updateInfo(name, url = "") {
+  brand = name;
+  URL = url;
+}
+
+
 export function brandPage() {
-  return 1;
+  container.innerHTML = "";
+
+  const top = document.createElement('div');
+  top.classList = 'flex items-center h-14 mt-10';
+  top.id = 'top';
+
+  const topImg1 = document.createElement('img');
+  topImg1.classList = 'w-24 h-auto';
+  topImg1.src = '../src/assets/icons/prev icon.png';
+  topImg1.alt = '_';
+
+  const topText = document.createElement('p');
+  topText.classList = 'font-bold text-3xl pb-1';
+  topText.textContent = brand;
+
+  // Append top images and text to top div
+  top.appendChild(topImg1);
+  top.appendChild(topText);
+  container.appendChild(top);
+
+  const items = document.createElement('div');
+  items.classList = 'mt-6 px-8 grid grid-cols-2 mb-32';
+  items.id = 'items';
+
+  container.appendChild(items);
+  console.log('as')
+  getProducts(MOST_URL)
+
 }
